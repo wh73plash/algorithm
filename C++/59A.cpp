09f0997232f09d8c113f0db4e261a962fa77f113
JeinKim <wh73plash@gmail.com>
2022-04-02 18:11:32 +0900

    22 April 02 18:11

---
+++ C++/59A.cpp
@@ -0,0 +1,90 @@
+//#pragma region include
+//#pragma warning(disable : 4996)
+//#include <cassert>
+//#include <cctype>
+//#include <cerrno>
+//#include <cfloat>
+//#include <ciso646>
+//#include <climits>
+//#include <clocale>
+//#include <cmath>
+//#include <csetjmp>
+//#include <csignal>
+//#include <cstdarg>
+//#include <cstddef>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <cstring>
+//#include <ctime>
+//#include <algorithm>
+//#include <bitset>
+//#include <complex>
+//#include <deque>
+//#include <exception>
+//#include <fstream>
+//#include <functional>
+//#include <iomanip>
+//#include <ios>
+//#include <iosfwd>
+//#include <iostream>
+//#include <istream>
+//#include <iterator>
+//#include <limits>
+//#include <list>
+//#include <locale>
+//#include <map>
+//#include <memory>
+//#include <new>
+//#include <numeric>
+//#include <ostream>
+//#include <queue>
+//#include <set>
+//#include <sstream>
+//#include <stack>
+//#include <stdexcept>
+//#include <streambuf>
+//#include <string>
+//#include <typeinfo>
+//#include <utility>
+//#include <valarray>
+//#include <vector>
+//#pragma endregion
+//
+//using namespace std;
+//
+//inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v") {
+//    s.erase(0, s.find_first_not_of(t));
+//    return s;
+//}
+//
+//inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v") {
+//    s.erase(s.find_last_not_of(t) + 1);
+//    return s;
+//}
+//
+//inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v") {
+//    return ltrim(rtrim(s, t), t);
+//}
+//
+//int32_t main(const int argc, const char** argv, const char** env) {
+//    std::ios::sync_with_stdio(false);
+//    std::cin.tie(nullptr);
+//    std::cout.tie(nullptr);
+//
+//    std::string str;
+//    std::cin >> str;
+//    int uppercase = 0, lowercase = 0;
+//    for(const char& i : str){
+//        if(i >= 'a') ++lowercase;
+//        else ++uppercase;
+//    }
+//    for(char& i : str){
+//        if(uppercase > lowercase){
+//            if(i >= 'a') i -= 32;
+//        }else{
+//            if(i < 'a') i += 32;
+//        }
+//    }
+//    std::cout << str << '\n';
+//    return EXIT_SUCCESS;
+//}
